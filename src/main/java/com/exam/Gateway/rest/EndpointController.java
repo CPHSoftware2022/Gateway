1019b8ab15a11ea45390c4db2e7333cb9a8a40b4
MiaSimone <cph-md291@cphbusiness.dk>
2022-12-07 11:06:18 +0100

    working on gateway customer

---
+++ src/main/java/com/exam/Gateway/rest/EndpointController.java
@@ -0,0 +1,36 @@
+package com.exam.Gateway.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/api")
+public class EndpointController {
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    @GetMapping("/customer-service/test")
+    public ResponseEntity<Object> getDataCustomerTest() {
+        String url = "http://localhost:8081/test";
+        ResponseEntity<String> responseEntity = restTemplate.getForEntity(url, String.class);
+        return ResponseEntity.ok(responseEntity.getBody());
+    }
+    @GetMapping("/customer/{id}")
+    public ResponseEntity<Object> getCustomerById(@PathVariable("id") Long id){
+        String url = "http://localhost:8081/api/customer/" + id;
+        ResponseEntity<String> responseEntity = restTemplate.getForEntity(url, String.class);
+        return ResponseEntity.ok(responseEntity.getBody());
+    }
+    @GetMapping("/customers")
+    public ResponseEntity<Object> getCustomers(){
+        String url = "http://localhost:8081/api/customers";
+        ResponseEntity<String> responseEntity = restTemplate.getForEntity(url, String.class);
+        return ResponseEntity.ok(responseEntity.getBody());
+    }
+}
